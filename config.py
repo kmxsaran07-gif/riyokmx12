4f0fc408a041c459eebe7ded138b25e4fd5f3ad8
kmxsaran07-gif <kmxsaran07@gmail.com>
2025-11-30 23:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "28446111"
+api_hash = "4ef8f43ed7d3f22b1e3acc40e86d7506"
+bot_token = "8203492782:AAH_9gkXAiKfd5sOnE_4kQa1dNdeuyjAk_0"
+auth_users = [1956027694]
+sudo_users = [1956027694]
