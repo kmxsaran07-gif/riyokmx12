5ab00dc9a3e1b3e1f77c1f1c3c74dfd6f1731b39
kmxsaran07-gif <kmxsaran07@gmail.com>
2025-12-02 17:30:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "28446111"
 api_hash = "4ef8f43ed7d3f22b1e3acc40e86d7506"
-bot_token = "8203492782:AAH_9gkXAiKfd5sOnE_4kQa1dNdeuyjAk_0"
-auth_users = [1956027694]
-sudo_users = [1956027694]
+bot_token = "8264874704:AAF6Eq3Zy4xaNUU5nphq2dPkbrPPZJEvF7g"
+auth_users = [1696736955]
+sudo_users = [1696736955]
